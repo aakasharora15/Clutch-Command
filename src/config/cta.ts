97eacbbeb76cd58f6dafda076d30c5c4675b33ea
Aakash Arora <aakasharora.work@gmail.com>
2026-08-10 23:08:20 +0100

    Update CTA link to Stripe payment URL

--- src/config/cta.ts
+++ src/config/cta.ts
@@ -3,7 +3,7 @@
  * Change these values once to update every button across the entire site.
  */
 export const CTA = {
-  url: 'https://buy.stripe.com/test_placeholder',
+  url: 'https://stripe.com/gb/payments/onelink',
   label: 'Join the Academy',
   labelArrow: 'Join the Academy \u2192',
 } as const;
